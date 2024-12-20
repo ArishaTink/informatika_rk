678fa89a10b679425d36d46dbb29ff494c9ab523
Arisha <selestinatink@mail.ru>
2024-12-20 12:10:49 +0300

    git init

---
+++ rk_informatika.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <bitset>
+#include <string>
+//Иванцова Арина ИУ1-12Б
+
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+
+    string A = "101101";
+    string B = "110011";
+
+    int A_decimal = stoi(A, nullptr, 2);
+    int B_decimal = stoi(B, nullptr, 2);
+
+    int and_result = A_decimal & B_decimal;
+
+    int sum_result = A_decimal + B_decimal;
+
+    cout << "Побитовая операция AND: " << bitset<6>(and_result) << endl;
+    cout << "Сложение чисел A и B: " << bitset<8>(sum_result) << endl;
+
+    return 0;
+}
+
